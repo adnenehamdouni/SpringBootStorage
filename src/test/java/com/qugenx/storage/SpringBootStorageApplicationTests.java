ab05e9d17ca269f129389c783510c710b266001b
Adnene HAMDOUNI <adnene.hamdouni@qugenx.com>
2019-11-30 10:27:08 +0100

    first commit

---
+++ src/test/java/com/qugenx/storage/SpringBootStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qugenx.storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
